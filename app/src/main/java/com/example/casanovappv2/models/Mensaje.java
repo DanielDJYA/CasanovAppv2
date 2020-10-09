27d16d42163a76f71a995c6dd8e7241e9281447d
user1 <yanez.31.12.98@gmail.com>
2020-10-09 15:05:17 -0500

    Subiendo Primera Muestra correccion1

---
+++ app/src/main/java/com/example/casanovappv2/models/Mensaje.java
@@ -0,0 +1,23 @@
+package com.example.casanovappv2.models;
+
+public class Mensaje {
+    private String texto;
+
+    public Mensaje(String mensaje) {
+    }
+
+    public void Mensaje(String texto){
+
+        this.texto=texto;
+    }
+
+    public String getTexto() {
+
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+
+        this.texto = texto;
+    }
+}
